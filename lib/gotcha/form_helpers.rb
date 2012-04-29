0ee9aa527a4915133103780197ddc92cb87c23ae
Jack Brown <jack@brownjohnf.com>
2012-04-29 16:13:24 +0000

    set it to only use sum gotcha

--- lib/gotcha/form_helpers.rb
+++ lib/gotcha/form_helpers.rb
@@ -6,7 +6,8 @@ module Gotcha
     def gotcha(options = {})
       options[:label_options] ||= {}
       options[:text_field_options] ||= {}
-      if gotcha = Gotcha.random
+      # options[:gotcha_type] ||= :sum
+      if gotcha = Gotcha.SumGotcha #random
         field = "gotcha_response[#{gotcha.class.name.to_s}-#{Digest::MD5.hexdigest(gotcha.class.down_transform(gotcha.answer))}]"
         (label_tag field, gotcha.question, options[:label_options]) + "\n" + (text_field_tag field, nil, options[:text_field_options])
       else
